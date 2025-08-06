0629cac8d774f44980c2fa0c5263daea0bda5c51
TothAdam0012 <tothadam0012@gmail.com>
2025-08-06 21:30:51 +0200

    interactive map test

--- assets/script/mob_stats.js
+++ assets/script/mob_stats.js
@@ -1,39 +1,63 @@
+const mainElementId = "main-content";
 const statPlaceholder = "???";
 const statHighlightClass = "mob-highlight";
 const itemDropsPlaceholder = "???, ???, ???";
 const linkHighlightClass = "mob-link-highlight";
+const mobDropsClass = "mob-drops";
+
+const mobImageClass = "mob-image";
+const mobImagePlaceholder = "/assets/img/default_mob.png";
+const mobDescFlexClass = "mob-div-description-flex";
+const mobDescClass = "mob-div-description";
+
 
 const isObject = (val) => typeof val === 'object' && val !== null && !Array.isArray(val);
-const getStatValue = (stat, defaultVal = statPlaceholder) => {
-	if(typeof mobStats[stat] === 'undefined') {
+const isString = (value) => typeof value === 'string' || value instanceof String;
+const getValue = (obj, property, defaultVal = statPlaceholder) => {
+	if(typeof obj[property] === 'undefined') {
 		return defaultVal;
 	}
-	return mobStats[stat];
-};
-const getDropValue = (stat, defaultVal = statPlaceholder) => {
-	if(typeof drops[stat] === 'undefined') {
-		return defaultVal;
-	}
-	return drops[stat];
+	return obj[property];
 };
 const hlSpan = (val) => '<span class="' + statHighlightClass + '">' + val + '</span>';
 const statsLabel = (val) => '<h3><u>' + val + '</u>:</h3><br>';
 
 
-function renderDrops() {
-	const mobDropsTableElement = document.getElementById("mob-drop-info");
-	if(!mobDropsTableElement) {
+function addImage() {
+	const mainContentElement = document.getElementById(mainElementId);
+	if(!mainContentElement) {
 		return;
 	}
+
+	const image = document.createElement('img');
+	image.classList.add(mobImageClass);
+	if(typeof mobImage === 'undefined' || !isString(mobImage)) {
+		image.setAttribute('src', mobImagePlaceholder);
+		mainContentElement.appendChild(image);
+		return;
+	}
+
+	image.setAttribute('src', mobImage);
+	image.setAttribute('onerror', "this.onerror=null; this.src='" + mobImagePlaceholder + "';");
+	mainContentElement.appendChild(image);
+}
+
+function renderDrops() {
+	const mainContentElement = document.getElementById(mainElementId);
+	if(!mainContentElement) {
+		return;
+	}
+
+	const mobDropsTableElement = document.createElement('table');
 
 	// process mob kill rewards
 	let points = statPlaceholder;
 	let exp = statPlaceholder;
 	let money = statPlaceholder;
-	if(isObject(drops)) {
-		points = getDropValue('points');
-		exp = getDropValue('exp');
-		money = getDropValue('money');
+	if(typeof drops !== 'undefined' && isObject(drops)) {
+		points = getValue(drops, 'points');
+		exp = getValue(drops, 'exp');
+		money = getValue(drops, 'money');
 	}
 	
 	// process item drops
@@ -48,72 +72,92 @@ function renderDrops() {
 
 
 	// create drops table
-	mobDropsTableElement.innerHTML = '<tr><td><h3>Drop:</h3></td><td><h3>Items:</h3></td></tr><tr><td><p>Social credit: ' + hlSpan(points) + '</p><p>Exp: ' + hlSpan(exp) + '</p><p>Moner: ' + hlSpan(money) + '</p></td><td><p class="' + statHighlightClass + '">' + itemDropsContent + '</p></td></tr>';
+	mobDropsTableElement.innerHTML = '<tr><td><h3>Drop:</h3></td><td><h3>Items:</h3></td></tr><tr><td class="' + mobDropsClass + '"><p>Social credit: ' + hlSpan(points) + '</p><p>Exp: ' + hlSpan(exp) + '</p><p>Moner: ' + hlSpan(money) + '</p></td><td><p class="' + statHighlightClass + '">' + itemDropsContent + '</p></td></tr>';
+	mainContentElement.appendChild(document.createElement('br'));
+	mainContentElement.appendChild(mobDropsTableElement);
 }
 
 function renderOtherInfo() {
-	const infoTableElement = document.getElementById("other-mob-info");
-	if(!infoTableElement) {
+	const mainContentElement = document.getElementById(mainElementId);
+	if(!mainContentElement) {
 		return;
 	}
 
 	if(typeof otherInfo === 'undefined' || !Array.isArray(otherInfo) || otherInfo.length < 1) {
-		infoTableElement.style.display = 'none';
 		return;
 	}
-	
+
+	let infoTableElement = document.createElement('table');
 	let infoList = "";
 	for(let i = 0; i < otherInfo.length; i++) {
 		infoList += "<li>" + otherInfo[i] + "</li>";
 	}
 	
 	infoTableElement.innerHTML = '<tr><td><h3>Other</h3></td></tr><tr><td><ul>' + infoList + '</ul></td></tr>';
+	mainContentElement.appendChild(document.createElement('br'));
+	mainContentElement.appendChild(infoTableElement);
 }
 
 function renderStats() {
-	const statsElement = document.getElementById("mob-stats");
-	if(!statsElement) {
+	const mainContentElement = document.getElementById(mainElementId);
+	if(!mainContentElement) {
 		return;
 	}
 
-	if(!isObject(mobStats)) {
-		statsElement.innerHTML = '<p class="' + statHighlightClass + '">' + statPlaceholder + '</p>';
-		return;
-	}
+	const flexDivElement = document.createElement('div');
+	flexDivElement.classList.add(mobDescFlexClass);
+	const descDivElement = document.createElement('div');
+	descDivElement.classList.add(mobDescClass);
 
+	let statsObj = typeof mobStats === 'undefined' || !isObject(mobStats) ? {} : mobStats;
 	let statsHTML = '';
-	statsHTML += '<h2>' + getStatValue('mobName') + '</h2><br>';
-	statsHTML += '<p>Type: ' + hlSpan(getStatValue('mobType')) + '</p>';
+	statsHTML += '<h2>' + getValue(statsObj, 'mobName') + '</h2><br>';
+
+	// add links to location(s) if exists
+	if(typeof mobLoc === 'undefined' || !Array.isArray(mobLoc) || mobLoc.length < 1) {
+		statsHTML += '<p>Location: ' + hlSpan(statPlaceholder) + '</p>';
+	} else {
+		let mobLocContent = addLinkIfExists(mobLoc[0], linkHighlightClass);
+		for(let i = 1; i < mobLoc.length; i++) {
+			mobLocContent += ", " + addLinkIfExists(mobLoc[i], linkHighlightClass);
+		}
+		statsHTML += '<p>Location: ' + hlSpan(mobLocContent) + '</p>';
+	}
+
+	statsHTML += '<p>Type: ' + hlSpan(getValue(statsObj, 'mobType')) + '</p>';
 
 	statsHTML += statsLabel("Stats");
-	statsHTML += '<p>Health: ' + hlSpan(getStatValue('health')) + '</p>';
-	let armor = getStatValue('armor', 0);
-	let toughness = getStatValue('toughness', 0);
+	statsHTML += '<p>Health: ' + hlSpan(getValue(statsObj, 'health')) + '</p>';
+	let armor = getValue(statsObj, 'armor', 0);
+	let toughness = getValue(statsObj, 'toughness', 0);
 	statsHTML += '<p>Defense: ' + hlSpan(armor*2 + toughness*3) + ' % (' + hlSpan(armor) + ' armor + ' + hlSpan(toughness) + ' toughness)</p>';
-	statsHTML += '<p>Absolute def: ' + hlSpan(getStatValue('absoluteDef')) + '</p>';
+	statsHTML += '<p>Absolute def: ' + hlSpan(getValue(statsObj, 'absoluteDef')) + '</p>';
 
 	statsHTML += statsLabel("Attack value");
-	statsHTML += '<p>Damage: ' + hlSpan(getStatValue('damage')) + '</p>';
-	statsHTML += '<p>Armor pierce: ' + hlSpan(getStatValue('armorPierce')) + ' %</p>';
-	statsHTML += '<p>Enchant pierce: ' + hlSpan(getStatValue('enchantPierce')) + ' %</p>';
-	statsHTML += '<p>Shield pierce: ' + hlSpan(getStatValue('shieldPierce')) + ' %</p>';
-	statsHTML += '<p>Shield cooldown: ' + hlSpan(getStatValue('shieldCooldown')) + ' sec</p>';
+	statsHTML += '<p>Damage: ' + hlSpan(getValue(statsObj, 'damage')) + '</p>';
+	statsHTML += '<p>Armor pierce: ' + hlSpan(getValue(statsObj, 'armorPierce')) + ' %</p>';
+	statsHTML += '<p>Enchant pierce: ' + hlSpan(getValue(statsObj, 'enchantPierce')) + ' %</p>';
+	statsHTML += '<p>Shield pierce: ' + hlSpan(getValue(statsObj, 'shieldPierce')) + ' %</p>';
+	statsHTML += '<p>Shield cooldown: ' + hlSpan(getValue(statsObj, 'shieldCooldown')) + ' sec</p>';
 
 	statsHTML += statsLabel("Resistances");
-	statsHTML += '<p>Crit resistance: ' + hlSpan(getStatValue('resCrit')) + ' %</p>';
-	statsHTML += '<p>Protection: ' + hlSpan(getStatValue('resEnv')) + ' %</p>';
-	statsHTML += '<p>Projectile protection: ' + hlSpan(getStatValue('resProj')) + ' %</p>';
-	statsHTML += '<p>Blast protection: ' + hlSpan(getStatValue('resExpl')) + ' %</p>';
-	statsHTML += '<p>Fire protection: ' + hlSpan(getStatValue('resFire')) + ' %</p>';
-	statsHTML += '<p>Fall protection: ' + hlSpan(getStatValue('resFall')) + ' %</p>';
+	statsHTML += '<p>Crit resistance: ' + hlSpan(getValue(statsObj, 'resCrit')) + ' %</p>';
+	statsHTML += '<p>Protection: ' + hlSpan(getValue(statsObj, 'resEnv')) + ' %</p>';
+	statsHTML += '<p>Projectile protection: ' + hlSpan(getValue(statsObj, 'resProj')) + ' %</p>';
+	statsHTML += '<p>Blast protection: ' + hlSpan(getValue(statsObj, 'resExpl')) + ' %</p>';
+	statsHTML += '<p>Fire protection: ' + hlSpan(getValue(statsObj, 'resFire')) + ' %</p>';
+	statsHTML += '<p>Fall protection: ' + hlSpan(getValue(statsObj, 'resFall')) + ' %</p>';
 
 	statsHTML += '<br>';
-	statsHTML += '<p>Sword resistance: ' + hlSpan(getStatValue('resSword')) + ' %</p>';
+	statsHTML += '<p>Sword resistance: ' + hlSpan(getValue(statsObj, 'resSword')) + ' %</p>';
 	
 	
-	statsElement.innerHTML = statsHTML;
+	descDivElement.innerHTML = statsHTML;
+	flexDivElement.appendChild(descDivElement);
+	mainContentElement.appendChild(flexDivElement);
 }
 
+addImage();
 renderStats();
 renderDrops();
 renderOtherInfo();
